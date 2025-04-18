d128ad3b1d568ab1ebb53e0ba44c2cc8d24f4f86
Heloise18 <heloise.zelma18@gmail.com>
2025-04-18 17:17:33 -0300

    Primeiros passos Função desenhar tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,3 +1,41 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// ─ │ ┌ ┐└ ┘├ ┤┬ ┴ ┼
+
+
+int[,] array = new int [20,20];
+array[1,2]=1;
+array[5,6]=0;
+desenhar(array);
+
+void desenhar(int[,] tabulerio)
+{
+    Console.WriteLine("┌──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┬──┐");
+    for(int l = 0; l<20; l++)
+    {
+        for(int c = 0; c<20; c++)
+        {
+            
+            if(tabulerio[l,c] == 0)
+            {
+                Console.Write("│  ");
+            }
+            else 
+            {
+                Console.Write("│██");
+            }
+            if(c==19)
+            {
+                Console.Write("│"); 
+            }
+        }
+
+        Console.WriteLine();
+        if(l<19)
+        {
+            Console.WriteLine("├──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┼──┤");
+        }
+    }
+
+    Console.WriteLine("└──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┘");
+
+}
 
